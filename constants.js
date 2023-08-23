0bb9da9c598b9ae9b8fa80ef321fd4488839a193
Ethan Groene <ethangroene@gmail.com>
2023-08-23 01:44:41 -0400

    Add auxiliary info for similar inputs

---
+++ constants.js
@@ -0,0 +1,21 @@
+// Export this into ClassTextInput
+export const inputInfo = [
+  {
+    id: "firstName",
+    label: "First Name",
+    placeholder: "Bilbo",
+    errorMessage: "First name must be at least 2 characters long",
+  },
+  {
+    id: "lastName",
+    label: "Last Name",
+    placeholder: "Baggins",
+    errorMessage: "Last name must be at least 2 characters long",
+  },
+  {
+    id: "email",
+    label: "Email",
+    placeholder: "bilbo-baggins@adventurehobbits.net",
+    errorMessage: "Email is Invalid",
+  },
+];
