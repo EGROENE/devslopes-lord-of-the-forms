12ff3d0a0912d807beb720c671ab2b2c9bf74229
Ethan Groene <ethangroene@gmail.com>
2023-08-25 19:06:38 -0400

    Del unneeded code

--- src/ClassApp/ClassPhoneInput.jsx
+++ src/ClassApp/ClassPhoneInput.jsx
@@ -2,36 +2,6 @@ import React from "react";
 import { phoneInputs } from "../../constants";
 
 class ClassPhoneInput extends React.Component {
-  /* constructor() {
-    super();
-    this.phoneInputsParentElement = React.createRef();
-  } */
-
-  /* componentDidMount() {
-    console.log(this.phoneInputsParentElement.current);
-    console.log(Array.from(this.phoneInputsParentElement.current.children));
-  } */
-
-  /* focusPhoneInputs = (e, input) => {
-    const phoneInputDOMElements = Array.from(
-      this.phoneInputsParentElement.current.children
-    );
-
-    console.log(phoneInputDOMElements);
-    const value = e.target.value;
-    console.log(value);
-    console.log(value.length);
-
-    const maxLength = input.maxLength;
-    console.log(maxLength);
-    console.log(phoneInputDOMElements[phoneInputDOMElements.indexOf(input)]);
-    console.log(
-      phoneInputDOMElements[phoneInputDOMElements.indexOf(input) + 1]
-    );
-
-    phoneInputDOMElements[phoneInputDOMElements.indexOf(input) + 1]?.focus();
-  }; */
-
   render() {
     const { userData, handlePhoneInput, phoneInputsParentElement } = this.props;
     return (
@@ -47,11 +17,6 @@ class ClassPhoneInput extends React.Component {
               minLength={input.minLength}
               maxLength={input.maxLength}
               onChange={handlePhoneInput(phoneInputs.indexOf(input))}
-              /* onChange={(e) => {
-                handlePhoneInput(phoneInputs.indexOf(input));
-                this.focusPhoneInputs(e, e.target);
-              }} */
-              //onChange={e => { this.functionOne(e); this.functionTwo() }}
             />
             {phoneInputs.indexOf(input) !== phoneInputs.length - 1 && " - "}
           </>
