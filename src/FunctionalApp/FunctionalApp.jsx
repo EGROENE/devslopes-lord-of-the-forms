3559a8b57eff2a703c1bfb96df540344e6c7bbca
Ethan Groene <ethangroene@gmail.com>
2023-08-28 17:40:28 -0400

    Init state

--- src/FunctionalApp/FunctionalApp.jsx
+++ src/FunctionalApp/FunctionalApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import { ProfileInformation } from "../ProfileInformation";
 import { FunctionalForm } from "./FunctionalForm";
 
 export const FunctionalApp = () => {
+  const { userData, setUserData } = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    phone: ["", "", "", ""],
+    city: "Hobbiton",
+  });
+  const { inputErrors, setInputErrors } = useState({
+    emailError: true,
+    firstName: true,
+    lastNameError: true,
+    phoneError: true,
+    cityError: true,
+  });
+  const { attemptedSubmissionTally, setAttemptedSubmissionTally } = useState(0);
+  const { successfulSubmissionTally, setSuccessfulSubmissionTally } =
+    useState(0);
   return (
     <>
       <h2>Functional</h2>
