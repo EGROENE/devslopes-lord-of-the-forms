d0a55babcb8061a7600dc6369de5dda0fe46b076
Ethan Groene <ethangroene@gmail.com>
2023-08-29 13:54:29 -0400

    Del unneeded code, add explanatory comments

--- src/FunctionalApp/FunctionalForm.jsx
+++ src/FunctionalApp/FunctionalForm.jsx
@@ -3,12 +3,6 @@ import { FunctionalTextInput } from "./FunctionalTextInput";
 import { allCities } from "../utils/all-cities";
 import { FunctionalPhoneInput } from "./FunctionalPhoneInput";
 
-/* const firstNameErrorMessage = "First name must be at least 2 characters long";
-const lastNameErrorMessage = "Last name must be at least 2 characters long";
-const emailErrorMessage = "Email is Invalid";
-const cityErrorMessage = "State is Invalid";
-const phoneNumberErrorMessage = "Invalid Phone Number"; */
-
 export const FunctionalForm = ({
   userData,
   inputErrors,
@@ -26,6 +20,7 @@ export const FunctionalForm = ({
         <h3>User Information Form</h3>
       </u>
 
+      {/* Text inputs (first/last names, email) */}
       <FunctionalTextInput
         handleChange={handleChange}
         setErrors={setErrors}
@@ -47,6 +42,7 @@ export const FunctionalForm = ({
         </select>
       </div>
 
+      {/* Phone inputs */}
       <div className="input-wrap">
         <label htmlFor="phone">Phone:</label>
         <FunctionalPhoneInput
@@ -61,21 +57,6 @@ export const FunctionalForm = ({
           show={inputErrors.phoneError}
         />
       )}
-
-      {/* <div className="input-wrap">
-        <label htmlFor="phone">Phone:</label>
-        <div id="phone-input-wrap">
-          <input type="text" id="phone-input-1" placeholder="55" />
-          -
-          <input type="text" id="phone-input-2" placeholder="55" />
-          -
-          <input type="text" id="phone-input-3" placeholder="55" />
-          -
-          <input type="text" id="phone-input-4" placeholder="5" />
-        </div>
-      </div>
-
-      <ErrorMessage message={phoneNumberErrorMessage} show={true} /> */}
 
       <input type="submit" value="Submit" />
     </form>
