b687c74584f9f904cb9e0b59461a6d605dcc0b5d
Ethan Groene <ethangroene@gmail.com>
2023-08-29 13:53:06 -0400

    Implement FunctionalText-, FunctionalPhoneInput components

--- src/FunctionalApp/FunctionalForm.jsx
+++ src/FunctionalApp/FunctionalForm.jsx
@@ -1,10 +1,13 @@
 import { ErrorMessage } from "../ErrorMessage";
+import { FunctionalTextInput } from "./FunctionalTextInput";
+import { allCities } from "../utils/all-cities";
+import { FunctionalPhoneInput } from "./FunctionalPhoneInput";
 
-const firstNameErrorMessage = "First name must be at least 2 characters long";
+/* const firstNameErrorMessage = "First name must be at least 2 characters long";
 const lastNameErrorMessage = "Last name must be at least 2 characters long";
 const emailErrorMessage = "Email is Invalid";
 const cityErrorMessage = "State is Invalid";
-const phoneNumberErrorMessage = "Invalid Phone Number";
+const phoneNumberErrorMessage = "Invalid Phone Number"; */
 
 export const FunctionalForm = ({
   userData,
@@ -18,40 +21,48 @@ export const FunctionalForm = ({
   phoneInputsParentElement,
 }) => {
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
       <u>
         <h3>User Information Form</h3>
       </u>
 
-      {/* first name input */}
-      <div className="input-wrap">
-        <label>{"First Name"}:</label>
-        <input placeholder="Bilbo" />
-      </div>
-      <ErrorMessage message={firstNameErrorMessage} show={true} />
-
-      {/* last name input */}
-      <div className="input-wrap">
-        <label>{"Last Name"}:</label>
-        <input placeholder="Baggins" />
-      </div>
-      <ErrorMessage message={lastNameErrorMessage} show={true} />
-
-      {/* Email Input */}
-      <div className="input-wrap">
-        <label>{"Email"}:</label>
-        <input placeholder="bilbo-baggins@adventurehobbits.net" />
-      </div>
-      <ErrorMessage message={emailErrorMessage} show={true} />
+      <FunctionalTextInput
+        handleChange={handleChange}
+        setErrors={setErrors}
+        resetErrors={resetErrors}
+        attemptedSubmissionTally={attemptedSubmissionTally}
+        inputErrors={inputErrors}
+        userData={userData}
+      />
 
       {/* City Input */}
       <div className="input-wrap">
-        <label>{"City"}:</label>
-        <input placeholder="Hobbiton" />
+        <label htmlFor="city">City:</label>
+        <select name="city" id="city" onChange={(e) => handleChange(e, "city")}>
+          {allCities.map((city) => (
+            <option key={city} selected={city === userData.city}>
+              {city}
+            </option>
+          ))}
+        </select>
       </div>
-      <ErrorMessage message={cityErrorMessage} show={true} />
 
       <div className="input-wrap">
+        <label htmlFor="phone">Phone:</label>
+        <FunctionalPhoneInput
+          phoneInputsParentElement={phoneInputsParentElement}
+          userData={userData}
+          handlePhoneInput={handlePhoneInput}
+        />
+      </div>
+      {attemptedSubmissionTally > 0 && (
+        <ErrorMessage
+          message="Invalid Phone Number"
+          show={inputErrors.phoneError}
+        />
+      )}
+
+      {/* <div className="input-wrap">
         <label htmlFor="phone">Phone:</label>
         <div id="phone-input-wrap">
           <input type="text" id="phone-input-1" placeholder="55" />
@@ -64,7 +75,7 @@ export const FunctionalForm = ({
         </div>
       </div>
 
-      <ErrorMessage message={phoneNumberErrorMessage} show={true} />
+      <ErrorMessage message={phoneNumberErrorMessage} show={true} /> */}
 
       <input type="submit" value="Submit" />
     </form>
