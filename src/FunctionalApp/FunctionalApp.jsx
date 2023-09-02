3392bdf27860264ae888a9789f8c59fd4694dca7
Ethan Groene <ethangroene@gmail.com>
2023-09-02 13:49:36 -0400

    Define state & methods pertinent to form, in form comp
    Helps de-clutter parent comp (FunctionalApp.jsx)

--- src/FunctionalApp/FunctionalApp.jsx
+++ src/FunctionalApp/FunctionalApp.jsx
@@ -1,17 +1,8 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { ProfileInformation } from "../ProfileInformation";
 import { FunctionalForm } from "./FunctionalForm";
-import { containsOnlyDigits } from "../utils/validations";
 
 export const FunctionalApp = () => {
-  const phoneInputsParentElement = useRef(0);
-  const [userData, setUserData] = useState({
-    email: "",
-    firstName: "",
-    lastName: "",
-    phone: ["", "", "", ""],
-    city: "Hobbiton",
-  });
   const [prevUserData, setPrevUserData] = useState({
     email: "",
     firstName: "",
@@ -19,125 +10,24 @@ export const FunctionalApp = () => {
     phone: "",
     city: "",
   });
-  const [inputErrors, setInputErrors] = useState({
-    emailError: true,
-    firstNameError: true,
-    lastNameError: true,
-    phoneError: true,
-  });
   const [attemptedSubmissionTally, setAttemptedSubmissionTally] = useState(0);
   const [successfulSubmissionTally, setSuccessfulSubmissionTally] = useState(0);
-
-  // Method to update state value onChange of text, select inputs
-  const handleChange = (e, inputType) => {
-    const value = e.target.value;
-    setUserData((prevState) => {
-      return { ...prevState, [`${inputType}`]: value };
-    });
-  };
-
-  const handlePhoneInput = (index) => (e) => {
-    const value = e.target.value;
-    if (containsOnlyDigits(value)) {
-      const newPhoneState = userData.phone.map((phoneInput, phoneInputIndex) =>
-        index === phoneInputIndex ? value : phoneInput
-      );
-
-      // Set phone array in state to updated state array
-      setUserData((prevState) => {
-        return { ...prevState, phone: newPhoneState };
-      });
-
-      // If length of string containing only the digits in userData.phone is not equal to 6 (account for delay in setting of state above), set inputErrors.phone to true; else, to false:
-      if (userData.phone.toString().replace(/,/g, "").length !== 6) {
-        setErrors("phone");
-      } else {
-        resetErrors("phone");
-      }
-
-      // Logic to autoskip back & forth b/t phone-input fields:
-      const phoneInputDOMElements = Array.from(
-        phoneInputsParentElement.current.children
-      );
-
-      if (value.length === e.target.maxLength) {
-        phoneInputDOMElements[
-          phoneInputDOMElements.indexOf(e.target) + 1
-        ]?.focus();
-      } else if (value === "") {
-        phoneInputDOMElements[
-          phoneInputDOMElements.indexOf(e.target) - 1
-        ]?.focus();
-      }
-    }
-  };
-
-  const setErrors = (inputType) => {
-    setInputErrors((prevState) => {
-      return { ...prevState, [`${inputType}Error`]: true };
-    });
-  };
-
-  const resetErrors = (inputType) => {
-    setInputErrors((prevState) => {
-      return { ...prevState, [`${inputType}Error`]: false };
-    });
-  };
-
-  const handleSubmission = (e) => {
-    e.preventDefault();
-
-    const areNoErrors = Object.values(inputErrors).every(
-      (value) => value === false
-    );
-    // If no errors...
-    if (areNoErrors) {
-      setPrevUserData({
-        email: userData.email,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        phone: userData.phone,
-        city: userData.city,
-      });
-      setUserData({
-        email: "",
-        firstName: "",
-        lastName: "",
-        phone: ["", "", "", ""],
-        city: "Hobbiton",
-      });
-      setInputErrors({
-        emailError: true,
-        firstNameError: true,
-        lastNameError: true,
-        phoneError: true,
-      });
-      setAttemptedSubmissionTally(0);
-      setSuccessfulSubmissionTally(successfulSubmissionTally + 1);
-    } else {
-      // ELSE...
-      alert("Bad inputs.");
-      setAttemptedSubmissionTally(attemptedSubmissionTally + 1);
-    }
-  };
 
   return (
     <>
       <h2>Functional</h2>
       <ProfileInformation
         successfulSubmissionTally={successfulSubmissionTally}
+        setAttemptedSubmissionTally={setSuccessfulSubmissionTally}
         prevUserData={prevUserData}
+        setPrevUserData={setPrevUserData}
       />
       <FunctionalForm
-        userData={userData}
-        inputErrors={inputErrors}
-        handleChange={handleChange}
         attemptedSubmissionTally={attemptedSubmissionTally}
-        handleSubmission={handleSubmission}
-        setErrors={setErrors}
-        resetErrors={resetErrors}
-        handlePhoneInput={handlePhoneInput}
-        phoneInputsParentElement={phoneInputsParentElement}
+        setAttemptedSubmissionTally={setAttemptedSubmissionTally}
+        setPrevUserData={setPrevUserData}
+        successfulSubmissionTally={successfulSubmissionTally}
+        setSuccessfulSubmissionTally={setSuccessfulSubmissionTally}
       />
     </>
   );
--- src/FunctionalApp/FunctionalForm.jsx
+++ src/FunctionalApp/FunctionalForm.jsx
@@ -1,19 +1,128 @@
+import { useRef, useState } from "react";
 import { ErrorMessage } from "../ErrorMessage";
 import { FunctionalTextInput } from "./FunctionalTextInput";
 import { allCities } from "../utils/all-cities";
 import { FunctionalPhoneInput } from "./FunctionalPhoneInput";
+import { containsOnlyDigits } from "../utils/validations";
 
 export const FunctionalForm = ({
-  userData,
-  inputErrors,
-  handleChange,
   attemptedSubmissionTally,
-  handleSubmission,
-  setErrors,
-  resetErrors,
-  handlePhoneInput,
-  phoneInputsParentElement,
+  setAttemptedSubmissionTally,
+  setPrevUserData,
+  successfulSubmissionTally,
+  setSuccessfulSubmissionTally,
 }) => {
+  const phoneInputsParentElement = useRef(0);
+  const [userData, setUserData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    phone: ["", "", "", ""],
+    city: "Hobbiton",
+  });
+  const [inputErrors, setInputErrors] = useState({
+    emailError: true,
+    firstNameError: true,
+    lastNameError: true,
+    phoneError: true,
+  });
+  // Method to update state value onChange of text, select inputs
+  // Put in FunctionalForm.jsx:
+  const handleChange = (e, inputType) => {
+    const value = e.target.value;
+    setUserData((prevState) => {
+      return { ...prevState, [`${inputType}`]: value };
+    });
+  };
+
+  // Put in FunctionalForm.jsx:
+  const handlePhoneInput = (index) => (e) => {
+    const value = e.target.value;
+    if (containsOnlyDigits(value)) {
+      const newPhoneState = userData.phone.map((phoneInput, phoneInputIndex) =>
+        index === phoneInputIndex ? value : phoneInput
+      );
+
+      // Set phone array in state to updated state array
+      setUserData((prevState) => {
+        return { ...prevState, phone: newPhoneState };
+      });
+
+      // If length of string containing only the digits in userData.phone is not equal to 6 (account for delay in setting of state above), set inputErrors.phone to true; else, to false:
+      if (userData.phone.toString().replace(/,/g, "").length !== 6) {
+        setErrors("phone");
+      } else {
+        resetErrors("phone");
+      }
+
+      // Logic to autoskip back & forth b/t phone-input fields:
+      const phoneInputDOMElements = Array.from(
+        phoneInputsParentElement.current.children
+      );
+
+      if (value.length === e.target.maxLength) {
+        phoneInputDOMElements[
+          phoneInputDOMElements.indexOf(e.target) + 1
+        ]?.focus();
+      } else if (value === "") {
+        phoneInputDOMElements[
+          phoneInputDOMElements.indexOf(e.target) - 1
+        ]?.focus();
+      }
+    }
+  };
+
+  // Put in FunctionalForm.jsx:
+  const setErrors = (inputType) => {
+    setInputErrors((prevState) => {
+      return { ...prevState, [`${inputType}Error`]: true };
+    });
+  };
+
+  // Put in FunctionalForm.jsx:
+  const resetErrors = (inputType) => {
+    setInputErrors((prevState) => {
+      return { ...prevState, [`${inputType}Error`]: false };
+    });
+  };
+
+  // Put in FunctionalForm.jsx:
+  const handleSubmission = (e) => {
+    e.preventDefault();
+
+    const areNoErrors = Object.values(inputErrors).every(
+      (value) => value === false
+    );
+    // If no errors...
+    if (areNoErrors) {
+      setPrevUserData({
+        email: userData.email,
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        phone: userData.phone,
+        city: userData.city,
+      });
+      setUserData({
+        email: "",
+        firstName: "",
+        lastName: "",
+        phone: ["", "", "", ""],
+        city: "Hobbiton",
+      });
+      setInputErrors({
+        emailError: true,
+        firstNameError: true,
+        lastNameError: true,
+        phoneError: true,
+      });
+      setAttemptedSubmissionTally(0);
+      setSuccessfulSubmissionTally(successfulSubmissionTally + 1);
+    } else {
+      // ELSE...
+      alert("Bad inputs.");
+      setAttemptedSubmissionTally(attemptedSubmissionTally + 1);
+    }
+  };
   return (
     <form onSubmit={handleSubmission}>
       <u>
